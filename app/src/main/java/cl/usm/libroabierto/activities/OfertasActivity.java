c3f32ca956637ce199e9b9b853b9ef1650453383
slavkocs <slavko.cotoras@gmail.com>
2016-07-05 00:20:25 -0400

    Estructura de Ofertas agregada
    se agrega la estructura básica de la sección Ofertas

--- app/src/main/java/cl/usm/libroabierto/activities/OfertasActivity.java
+++ app/src/main/java/cl/usm/libroabierto/activities/OfertasActivity.java
@@ -1,5 +1,6 @@
 package cl.usm.libroabierto.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,12 +9,34 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cl.usm.libroabierto.R;
+import cl.usm.libroabierto.models.Book;
+import cl.usm.libroabierto.models.Usuario;
+import cl.usm.libroabierto.network.LibroAbiertoAPI;
+import cl.usm.libroabierto.network.LibroAbiertoClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
-public class OfertasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class OfertasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        Callback<List<Book>> {
 
     private NavigationView navMenu;
     private Menu lateral;
@@ -21,6 +44,9 @@ public class OfertasActivity extends AppCompatActivity implements NavigationView
     private MenuItem opcionPublicarLibro;
     private MenuItem opcionOfertas;
     private MenuItem opcionProfile;
+
+    ArrayList<Book> books = new ArrayList<>();
+    private BooksAdapter booksAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +78,94 @@ public class OfertasActivity extends AppCompatActivity implements NavigationView
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Offer Book List View
+        Retrofit retrofit = LibroAbiertoClient.getClient();
+        LibroAbiertoAPI api = retrofit.create(LibroAbiertoAPI.class);
+        Call<List<Book>> call = api.getBooks();
+        call.enqueue(this);
+
+        setListView();
+    }
+
+    /* ===============================
+    *    Offer Book List View Section
+    *  ===============================
+    */
+    private void setListView() {
+
+        ListView listViewLibros = (ListView) findViewById(R.id.bookListView);
+        booksAdapter = new BooksAdapter(this, R.layout.book_row_in_list, books);
+        listViewLibros.setAdapter(booksAdapter);
+
+        listViewLibros.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Load book Data for Offer Detail Activity
+                final Book selectedBook = (Book) adapterView.getAdapter().getItem(position);
+
+                Intent intent = new Intent(getApplicationContext(),
+                        OfertaDetailActivity.class);
+
+                intent.putExtra("BOOK_ID", selectedBook.getId());
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+        if(response.isSuccessful()){
+            books.addAll(response.body());
+            booksAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onFailure(Call<List<Book>> call, Throwable t) {
+        Log.d("RetrofitFailure", t.toString());
+    }
+
+    // Offer Book Adapter for Offer Book List
+    private class BooksAdapter extends ArrayAdapter<Book> {
+
+        public BooksAdapter(Context context, int resource, ArrayList<Book> objects) {
+            super(context, resource,R.id.titleBookTextView, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View row = super.getView(position, convertView, parent);
+
+            Book book = getItem(position);
+
+            final TextView publicadoTextView = (TextView)row.findViewById(R.id.publicadoBookTextView);
+            Retrofit retrofit = LibroAbiertoClient.getClient();
+            LibroAbiertoAPI api = retrofit.create(LibroAbiertoAPI.class);
+            //TODO Obtener id_usuario logeado con google
+            Call<Usuario> call = api.getUsuario("0");
+            call.enqueue(new Callback<Usuario>() {
+                @Override
+                public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                    publicadoTextView.setText(response.body().getNombre());
+                }
+
+                @Override
+                public void onFailure(Call<Usuario> call, Throwable t) {
+                    Log.d("RetrofitFailure", t.toString());
+                }
+            });
+
+            TextView fechaTextView = (TextView)row.findViewById(R.id.fechaPublicacionBookTextView);
+            fechaTextView.setText(book.getFecha_publicacion());
+
+            ImageView avatarImageView = (ImageView)row.findViewById(R.id.avatar);
+            Glide.with(OfertasActivity.this).load(book.getRuta_fotografia()).centerCrop().into(avatarImageView);
+
+            return row;
+        }
     }
 
     public boolean onNavigationItemSelected(MenuItem item) {
